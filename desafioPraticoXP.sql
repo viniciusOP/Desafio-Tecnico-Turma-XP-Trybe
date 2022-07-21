1b84bc320f91ff08812730ac47b1fc3676f38fde
viniciusOP <viniciusopassos@hotmail.com>
2022-07-21 13:20:26 -0300

    criar o banco de dados

---
+++ desafioPraticoXP.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS db_desafio_XP;
+
+CREATE DATABASE db_desafio_XP;
+
+USE db_desafio_XP;
+
+CREATE TABLE cliente (
+    id INT NOT NULL auto_increment,
+    saldo DOUBLE NOT NULL,
+    PRIMARY KEY(id)
+) ENGINE=INNODB;
+
+CREATE TABLE ativo (
+    id INT NOT NULL auto_increment,
+    quantidade INT NOT NULL,
+    valor DOUBLE NOT NULL,
+    PRIMARY KEY(id)
+) ENGINE=INNODB;
+
+CREATE TABLE ativo_cliente (
+    cliente_id INT NOT NULL,
+    ativo_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    valor_total DOUBLE NOT NULL,
+    FOREIGN KEY (cliente_id)
+        REFERENCES cliente (id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (ativo_id)
+        REFERENCES ativo (id)
+        ON DELETE CASCADE
+)  ENGINE=INNODB;
+
+SET SQL_SAFE_UPDATES = 0;
+
+INSERT INTO db_desafio_XP.cliente (valor) VALUES
+    (10.3000),
+    (20.5000),
+    (30.4000);
+
+INSERT INTO db_desafio_XP.ativo (quantidade, valor) VALUES
+    (50, 2.9000),
+    (70, 0.9000),
+    (100, 3.9000);
+
+INSERT INTO db_desafio_XP.ativo_cliente (cliente_id, ativo_id, quantidade, valor_total) VALUES
+    (1, 3, 5, 19.5000),
+    (2, 1, 10, 29.0000),
+    (3, 2, 15, 13.5000);
